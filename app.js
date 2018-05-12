c39d9eb928691de1fca5a5f7567381a9a73a8e75
cjharkins <charkins@nossi.edu>
2018-05-11 21:39:56 -0500

    added image

--- app.js
+++ app.js
@@ -16,17 +16,4 @@ app.get('/',(req,res)=>res.render('index'));
 app.get('/skills',(req,res)=>res.render('skills'));
 app.get('/jumbotron',(req,res)=>res.render('jumbotron'));
 
-
-app.get('/cards', (req,res)=> {
-	res.locals.hint = '';
-	res.locals.prompt = '';
-	res.render('cards'); 
-});
-
-app.get('/hello',(req,res)=>res.render('hello'));
-app.post('/hello',(req,res)=>{
-	console.dir(req.body);
-	res.render('hello')
-});
-
 app.listen(3000,()=>console.log('Server listening on 3000'));
